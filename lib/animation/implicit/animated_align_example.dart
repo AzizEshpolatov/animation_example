191defe543b3ead578d8d5c2acb69fe4c75b3bb9
Azizbek <aziz2311e@gmail.com>
2025-02-17 15:31:26 +0500

    I have written documentation on AnimatedAlign

---
+++ lib/animation/implicit/animated_align_example.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+/// animated Align b/n animation widget
+/// animated align bilan widgetni joylashuvini animation shaklda ozgartirishimz
+/// mumkin boladi,AnimatedAlign widgetidan foydalanamiz va bazi parametrlarini
+/// korib chiqishimiz mumkin.
+/// curve – Animatsiya harakat trayektoriyasi (easing)ni belgilaydi
+/// duration – Animatsiya qancha davom etishini belgilaydi masalan 1sekund.
+/// AnimatedAlign – Flutter vidjetlarining joylashuvini silliq o'zgartirish
+/// uchun ishlatiladigan qulay vosita bo'lib, u sodda animatsiyalar uchun juda
+/// mos keladi.
+
+class AnimatedAlignExample extends StatefulWidget {
+  const AnimatedAlignExample({super.key});
+
+  @override
+  State<AnimatedAlignExample> createState() => _AnimatedAlignExampleState();
+}
+
+class _AnimatedAlignExampleState extends State<AnimatedAlignExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
