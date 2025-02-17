a9f4e01b611b3347dfcbac24c75541626b1360a7
Azizbek <aziz2311e@gmail.com>
2025-02-17 15:39:27 +0500

    Example code for custom animation  has been written

--- lib/animation/custom/main_custom_animation.dart
+++ lib/animation/custom/main_custom_animation.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
 
-class MainCustomAnimation extends StatelessWidget {
+void main() {
+  runApp(const MaterialApp(home: MainCustomAnimation()));
+}
+
+class MainCustomAnimation extends StatefulWidget {
   const MainCustomAnimation({super.key});
 
   @override
+  State<MainCustomAnimation> createState() => _MainCustomAnimationState();
+}
+
+class _MainCustomAnimationState extends State<MainCustomAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _animation = Tween<double>(begin: 0, end: 300).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _startAnimation() {
+    if (_controller.isCompleted) {
+      _controller.reverse();
+    } else {
+      _controller.forward();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Custom Animation Example')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: _animation.value,
+              height: _animation.value,
+              color: Colors.blue,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _startAnimation,
+              child: const Text('Start Animation'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
